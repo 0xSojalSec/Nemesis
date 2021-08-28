4f94e9b1ec5dd8b6b2c08cf7f1ec9ec2aa988e8d
machinexa2 <machinexa>
2021-08-28 19:26:58 +0545

    Add unittests for better tool stability and early bug detection

---
+++ tests/test_init.py
@@ -0,0 +1,7 @@
+import unittest
+#["No input type='hidden case' test", "No input type='hidden case' test and name value change function"
+from Nemesis import Nemesis
+
+class NemesisTest(unittest.TestCase):
+    def test_test(self):
+        self.assertEqual(Nemesis, Nemesis, "Should be equal")
