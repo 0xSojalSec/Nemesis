0fa9e94f1e8abda90fb280c4e214b6ac96a996cc
machinexa2 <machinexa>
2021-09-01 13:32:17 +0545

    New release 1.2

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ pkg_resources.require('setuptools>=39.2')
 setuptools.setup(
     version = "1.0.2",
     url = "https://github.com/machinexa2/Nemesis",
-    download_url = 'https://github.com/machinexa2/Nemesis/archive/refs/tags/v3.0.tar.gz',
+    download_url = 'https://github.com/machinexa2/Nemesis/archive/refs/tags/v1.2.tar.gz',
 )
